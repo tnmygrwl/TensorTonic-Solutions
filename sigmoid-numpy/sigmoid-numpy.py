8ebb7ca1a21f52598186cd511918593d40a0cece
Tanmay Agrawal <tanmay7270@gmail.com>
2026-08-01 19:31:22 -0700

    Solve sigmoid-numpy

--- sigmoid-numpy/sigmoid-numpy.py
+++ sigmoid-numpy/sigmoid-numpy.py
@@ -4,7 +4,18 @@ def sigmoid(x):
     """
     Vectorized sigmoid function.
     """
-    neg_x = -np.array(x)
-    return 1./(1. + np.exp(neg_x))
 
+    # process both -x_i and +x_i seperately for numerical stability
+
+    values = np.asarray(x, dtype=np.float64)
+
+    result = np.empty_like(values)
+
+    pos_mask = values >=0
+    neg_mask = ~pos_mask
+
+    result[pos_mask] =  1./(1. + np.exp(-values[pos_mask]))
+    result[neg_mask] = np.exp(values[neg_mask]) /( 1.+np.exp(values[neg_mask]))
+
+    return result
     
